a08d42a1fd4971d0a728de2e940e8fc354577a3a
Hemanth sai Somaraju <hemanthsaisomaraju@gmail.com>
2025-06-05 23:14:26 +0530

    added be connectivity to signin and signup and implemented such that the token is stored in brower

--- src/components/ui/SigninPage.tsx
+++ src/components/ui/SigninPage.tsx
@@ -1,14 +1,33 @@
+import axios from "axios";
 import { InputComponent } from "../CreateContentModal";
 import Button from "./Button";
+import { BACKEND_URL } from "../../../config";
+import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import DashBoard from "../Dashboard";
 
 export default function SigninPage(){
+    const usernameRef=useRef<HTMLInputElement>(null);
+    const passwordRef=useRef<HTMLInputElement>(null);
+    const navigate=useNavigate();
+    async function handleSignin(){
+        const username=usernameRef.current?.value;
+        const password=passwordRef.current?.value;
+        console.log(username);
+        const response=await axios.post(BACKEND_URL+"/api/v1/signin",{
+            username,password
+        })
+        const jwt=response.data.token;
+        localStorage.setItem('token',jwt);
+        navigate("/dashboard");
+    }
     return (
         <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
             <div className="bg-gray-400 rounded-xl p-15">
                 <InputComponent placeholder="Username"></InputComponent>
                 <InputComponent placeholder="Password"></InputComponent>
                 <div className="flex justify-center m-4 items-center">
-                    <Button fullwidth={true} onclick={()=>console.log("signin Button Clicked")} variant="secondary" text="Submit" size="md"></Button>
+                    <Button fullwidth={true} onclick={()=>handleSignin()} variant="secondary" text="Submit" size="md"></Button>
 
                 </div>
                 <div className="flex justify-between flex-wrap p-2">Not signed up yet?
--- src/components/ui/SignupPage.tsx
+++ src/components/ui/SignupPage.tsx
@@ -3,17 +3,21 @@ import { InputComponent } from "../CreateContentModal";
 import Button from "./Button";
 import axios from "axios";
 import {BACKEND_URL} from '../../../config'
+import { useNavigate } from "react-router-dom";
 export default function SignupPage(){
     const usernameRef=useRef<HTMLInputElement>(null);
     const passwordRef=useRef<HTMLInputElement>(null);
-    async function handleClick(){
+    const navigate=useNavigate();
+    async function handleSignup(){
         const username=usernameRef.current?.value;
         const password=passwordRef.current?.value;
         console.log(username);
         await axios.post(BACKEND_URL+"/api/v1/signup",{
             username,password
         })
-        alert("you have signed up")
+        alert("you have signed up");
+        navigate("/signin");
+
     }
     return (
         <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
@@ -21,7 +25,7 @@ export default function SignupPage(){
                 <InputComponent reference={usernameRef} placeholder="Username"></InputComponent>
                 <InputComponent reference={passwordRef} placeholder="Password"></InputComponent>
                 <div className="flex justify-center m-4 items-center">
-                    <Button fullwidth={true} onclick={()=>handleClick()} variant="secondary" text="Signup" size="md"></Button>
+                    <Button fullwidth={true} onclick={()=>handleSignup()} variant="secondary" text="Signup" size="md"></Button>
 
                 </div>
             </div>
