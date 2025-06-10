3b6e2542f2f87ff60e3dd4b8eac62206b4a0fa61
Hemanth sai Somaraju <hemanthsaisomaraju@gmail.com>
2025-06-10 23:28:54 +0530

    handled warning of linkedinEmbed

--- frontend/src/components/ui/embeds/LinkedInEmbed.tsx
+++ frontend/src/components/ui/embeds/LinkedInEmbed.tsx
@@ -13,11 +13,11 @@ function getLinkedInEmbedLink(normalLink:string):string {
 
 
 export function LinkedInEmbed(props:EmbedProps){
-    const embedLink=getLinkedInEmbedLink("https://www.linkedin.com/feed/update/urn:li:share:7334944262779723776");
+    const embedLink=getLinkedInEmbedLink(props.link);
     // const embedLink=getLinkedInEmbedLink(props.link);
     return <div>
 {/* <iframe src={"https://www.linkedin.com/embed/feed/update/urn:li:share:7334944262779723776"} height="50" width="50" frameBorder="0" allowFullScreen={true} title="Embedded post"></iframe> */}
-<iframe src="https://www.linkedin.com/embed/feed/update/urn:li:ugcPost:7322563857183313921?collapsed=1" height="654" width="504" frameBorder="0" allowFullScreen={true} title="Embedded post"></iframe>
+<iframe src={embedLink} height="654" width="504" frameBorder="0" allowFullScreen={true} title="Embedded post"></iframe>
 <script src="https://platform.linkedin.com/in.js" type="text/javascript">lang: en_US</script>
     </div>
 }
